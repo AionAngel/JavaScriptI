d08cb64fb925ac7c087002f187350029262ff0e4
AionAngel <lucas.augustus@gmail.com>
2017-07-03 17:38:05 -0300

    Fim da Aula 3 - Inicio Aula 4

--- js/principal.js
+++ js/principal.js
@@ -2,38 +2,49 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Lucas Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+for (var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+    console.log(paciente);
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
 
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
 
 
-var pesoEhvalido = true;
-var alturaEhvalido = true;
+    var pesoEhvalido = true;
+    var alturaEhvalido = true;
 
-if(peso <= 0 || peso>1000){
-    console.log("Peso Invalido");
-    tdImc.textContent = "Peso Invalido";
-    pesoEhvalido = false;
+    if(peso <= 0 || peso>1000){
+        console.log("Peso Invalido");
+        tdImc.textContent = "Peso Invalido";
+        pesoEhvalido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura<=0 || altura>=3){
+        console.log("Altura Invalida");
+        tdImc.textContent="Altura Invalida"
+        alturaEhvalido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhvalido && alturaEhvalido){
+
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
 }
 
-if(altura<=0 || altura>=3){
-    console.log("Altura Invalida");
-    tdImc.textContent="Altura Invalida"
-    alturaEhvalido = false;
-}
 
-if (pesoEhvalido && alturaEhvalido){
 
-    var imc = peso / (altura*altura);
-    tdImc.textContent = imc;
-}
 
