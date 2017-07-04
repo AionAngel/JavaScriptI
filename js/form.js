c10adae1c2a3327576e800c5cd24f1ac911130e2
AionAngel <lucas.augustus@gmail.com>
2017-07-04 19:11:04 -0300

    Fim da Aula 5 Inicio da Aula 6

--- js/form.js
+++ js/form.js
@@ -1,36 +1,20 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event) {
+
     event.preventDefault();
+
     var form = document.querySelector("#form-adiciona");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    var pacienteTr = montaTr(paciente);
 
     var tabela = document.querySelector("#tabela-pacientes");
+
     tabela.appendChild(pacienteTr);
+
+    form.reset();
 });
 
 function obtemPacienteDoFormulario(form) {
@@ -44,4 +28,28 @@ function obtemPacienteDoFormulario(form) {
     }
 
     return paciente;
+}
+
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+    return td;
+}
+
+function montaTr(paciente) {
+    //Cria tr
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add('paciente');
+
+    // Cria td e adiciona na tr
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, 'info-peso'));
+    pacienteTr.appendChild(montaTd(paciente.altura, 'info-altura'));
+    pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gordura'));
+    pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'));
+
+    //retorna a tr
+    return pacienteTr;
+
 }
