679699cdd803a29fb2161f8e4c299696fc8c1aa8
AionAngel <lucas.augustus@gmail.com>
2017-07-08 20:54:50 -0300

    Fim do curso

--- js/form.js
+++ js/form.js
@@ -4,27 +4,28 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     event.preventDefault();
 
+    vevent.preventDefault();
     var form = document.querySelector("#form-adiciona");
-
     var paciente = obtemPacienteDoFormulario(form);
-
-    var pacienteTr = montaTr(paciente);
-
-     var erros = validaPaciente(paciente);
-
-     if (erros.length > 0) {
-        
+    var erros = validaPaciente(paciente);
+    if (erros.length > 0) {
         exibeMensagensDeErro(erros);
         return;
-
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
+    adicionaPacienteNaTabela(paciente);
 
-    tabela.appendChild(pacienteTr);
-
-    form.reset();
+    form.reset();    
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
+
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form) {
 
