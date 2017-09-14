9d5deb13de7b89fb5dae183ecd60932dc7ba6a01
AionAngel <lucas.augustus@gmail.com>
2017-09-14 12:00:37 -0300

    Brincando no playground

--- playground.js
+++ playground.js
@@ -1,11 +1,35 @@
 console.log("Hello World!");
 
-var linha1 = document.querySelector(".numero");
-var tdNum1 = linha1.querySelector(".primeiro");
-var tdNum2 = linha1.querySelector(".segundo");
-var res = linha1.querySelector(".resultado");
+var linhas = document.querySelectorAll(".numero");
+
+//var tdNum1 = linha1.querySelector(".primeiro");
+//var tdNum2 = linha1.querySelector(".segundo");
+//var res = linha1.querySelector(".resultado");
 
 var num1 = tdNum1.textContent;
 var num2 = tdNum2.textContent;
 
-res.textContent = +num1 + +num2;
+for(var i = 0; i < linhas.length; i ++) {
+
+    console.log(linhas[i])
+}
+var num1EhValido = true;
+var num2EhValido = true;
+
+if (num1 > 2 || num1 < 0) {
+    console.log("Numero 1 Invalido");
+    res.textContent = "Numero 1 Invalido";
+    num1EhValido = false;
+}
+
+if (num2 > 2 || num2 < 0) {
+    console.log("Numero 1 Invalido");
+    res.textContent = "Numero 2 Invalido";
+    num2EhValido = false;
+}
+
+if (num1EhValido && num2EhValido) {
+    res.textContent = +num1 + +num2;
+}
+
+
