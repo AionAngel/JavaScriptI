af051d5f143953f260729224ef4a5fc861365f88
AionAngel <lucas.augustus@gmail.com>
2017-09-15 13:42:50 -0300

    Sexta feira antes da exposibram

--- playground.js
+++ playground.js
@@ -8,28 +8,27 @@ for (var i = 0; i < linhas.length; i ++) {
     var num2EhValido = true;
 
     var linhaAtual = linhas[i];
-    //var trAtual = linhaAtual.querySelector(".numero");
-    var tdNum1 = linhaAtual.querySelector(".primeiro");
-    var tdNum2 = linhaAtual.querySelector(".segundo");
+ 
+    var num1 = linhaAtual.querySelector(".primeiro").textContent;
+    var num2 = linhaAtual.querySelector(".segundo").textContent;
     var res = linhaAtual.querySelector(".resultado");
-
-    var num1 = tdNum1.textContent;
-    var num2 = tdNum2.textContent;
 
     if (num1 > 10 || num1 < 0) {
         console.log("Numero 1 Invalido");
         res.textContent = "Numero 1 Invalido";
         num1EhValido = false;
+        linhaAtual.classList.add("numero-invalido");
     }
     
     if (num2 > 10 || num2 < 0) {
-        console.log("Numero 1 Invalido");
+        console.log("Numero 2 Invalido");
         res.textContent = "Numero 2 Invalido";
         num2EhValido = false;
+        linhaAtual.classList.add("numero-invalido");
     }
     
     if (num1EhValido && num2EhValido) {
-        res.textContent = +num1 + +num2;
+        res.textContent = (+num1 + +num2).toFixed(2);
     }
     
 }
