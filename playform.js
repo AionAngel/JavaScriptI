eccc591de724bb123149e3abf18e50b0cc153ab4
AionAngel <lucas.augustus@gmail.com>
2017-09-22 15:23:52 -0300

    transformando tudo em funcao

--- playform.js
+++ playform.js
@@ -11,34 +11,53 @@ botao.addEventListener('click', function(event){
 
     //Lendo o formulario e os inputs do HTML
     var formulario = document.querySelector('#formulario');
-    var form1 = formulario.primeiro.value;
-    var form2 = formulario.segundo.value;
+    var dados = obtemNumerosForm(formulario);
 
-    //Criando elementos TR e TD para inserir na tabela
-    var elementoTr = document.createElement('tr');
-    var elementoTd1 = document.createElement('td');
-    var elementoTd2 = document.createElement('td');
-    var elementoTd3 = document.createElement('td');
+    //Cria TR
+    var pacienteTr = criaTr(dados);
+    
+     //Lê a TBODY do HTML
+     var tabela = document.querySelector('#tabela-pacientes');
 
-    //Adiciona as classes aos elementos criados
-    elementoTr.classList.add('numero');
-    elementoTd1.classList.add('primeiro');
-    elementoTd2.classList.add('segundo');
-    elementoTd3.classList.add('resultado');
-
-    //Insere o conteudo nas TDs
-    elementoTd1.textContent = form1;
-    elementoTd2.textContent = form2;
-    elementoTd3.textContent = somaDoisNumeros(form1,form2);
-
-    //Insere as TDs dentro da TR criada
-    elementoTr.appendChild(elementoTd1);
-    elementoTr.appendChild(elementoTd2);
-    elementoTr.appendChild(elementoTd3);
-
-    //Lê a TBODY do HTML
-    var tabela = document.querySelector('#tabela-pacientes');
-
-    //Insere o TR dentro do TBODY
-    tabela.appendChild(elementoTr);
+     //Insere o TR dentro do TBODY
+     tabela.appendChild(pacienteTr);
+    
     });
+
+    function obtemNumerosForm(formulario) {
+        var dados = {
+            num1: formulario.primeiro.value,
+            num2: formulario.segundo.value,
+            soma: somaDoisNumeros(formulario.primeiro.value,formulario.segundo.value)
+        }
+        return dados;
+    }
+
+    function criaTr(dados) {
+
+        //Criando elementos TR e TD para inserir na tabela
+        var elementoTr = document.createElement('tr');
+        elementoTr.classList.add('numero');
+
+        var elementoTd1 = criaTd(dados.num1,"primeiro");
+        var elementoTd2 = criaTd(dados.num2,"segundo");
+        var elementoTd3 = criaTd(dados.soma,"resultado");
+
+        //Insere as TDs dentro da TR criada
+        elementoTr.appendChild(elementoTd1);
+        elementoTr.appendChild(elementoTd2);
+        elementoTr.appendChild(elementoTd3);
+
+       
+        return elementoTr;
+       
+
+    }
+
+    function criaTd (dado,classe){
+        var td = document.createElement('td');
+        td.textContent = dado;
+        td.classList.add(classe);
+        
+        return td;
+    }
